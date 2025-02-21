ef91f261cb78436cfb359ba11e0bae3ed8b21ff2
secondHakan <ahmethakandemirci0@gmail.com>
2025-02-21 05:23:22 +0400

    homework.py

--- homework.py
+++ homework.py
@@ -1,3 +1,20 @@
+import math_1 as matematikislemleri
+from selenium import webdriver
+
+
+
+def Calculator (price, discount):
+    a = (price*discount) /100 
+    lastprice = price - a 
+    return lastprice
+
+
+NewPrice = Calculator(100, 20)
+
+print(NewPrice)
+
+
+
 print("1 - prof")
 print("2 - docent")
 print("3 - doktor")
@@ -28,3 +45,40 @@ class Calisanlar:
 
 calisan1 = Calisanlar(title , name, surname)
 calisan1.welcome_display()
+
+
+class Human:
+    name = "ahmet"
+    def __init__(self):
+        print("bir instance uretildi yani ornek")
+    def talk (self,sentence):
+        print(f"{self.name}: {sentence}")
+    
+    def walk (self, sentence): 
+        print(f"{self.name} {sentence}")
+
+human1 = Human()
+human1.name = "Osman"
+human1.talk("naber beyler")
+
+human2 = Human()
+human2.walk("hanimlar ben kosuyorum")
+
+
+class Car: 
+    speed = 12
+    def set_speed(self, speed):
+        self.speed = speed 
+
+    def drive (self):
+        print(f"driving at {self.speed} km/h")
+
+mycar = Car()
+mycar.drive()
+
+from selenium import webdriver
+
+
+chormeDriver = webdriver.Chrome()
+
+print(matematikislemleri.collection(12,3)) 
